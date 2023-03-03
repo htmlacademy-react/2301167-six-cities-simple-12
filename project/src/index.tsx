118019ec234f3bfb24a607d673c943b8c4b5ac07
MOROZOVcode <morozov.code@gmail.com>
2023-03-03 22:19:14 +0400

    Разбил страницы на компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Counters = {
+  AdsCount: 312,
+} as const;
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App adsCount={Counters.AdsCount} />
+  </React.StrictMode>
 );
