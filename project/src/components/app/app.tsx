4720704352a5c919ae0583ce0a6685abdf504cd2
MOROZOVcode <morozov.code@gmail.com>
2023-03-08 15:28:20 +0400

    Добавляет роутинг

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import PropertyPage from '../../pages/property-page/property-page';
+import Page404 from '../../pages/Page-404/Page404';
 
 type AppProps = {
   adsCount: number;
 };
 
 function App({ adsCount }: AppProps): JSX.Element {
-  return <MainPage adsCount={adsCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage adsCount={adsCount} />}
+        />
+        <Route path={AppRoute.Login} element={<LoginPage />} />
+        <Route path={AppRoute.Room} element={<PropertyPage />} />
+        <Route path='*' element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
