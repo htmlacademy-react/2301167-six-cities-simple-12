707e05de242e02a6e40cb823d09bb7c6de93736b
MOROZOVcode <morozov.code@gmail.com>
2023-03-08 16:13:27 +0400

    Делает из лого отдельный компонент

--- project/src/pages/login-page/login-page.tsx
+++ project/src/pages/login-page/login-page.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 export default function LoginPage() {
   return (
@@ -6,17 +7,7 @@ export default function LoginPage() {
       <header className='header'>
         <div className='container'>
           <div className='header__wrapper'>
-            <div className='header__left'>
-              <Link className='header__logo-link' to='main.html'>
-                <img
-                  className='header__logo'
-                  src='img/logo.svg'
-                  alt='6 cities logo'
-                  width='81'
-                  height='41'
-                />
-              </Link>
-            </div>
+            <Logo />
           </div>
         </div>
       </header>
--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import PlaceCard from '../../components/place-card/place-card';
+import Logo from '../../components/logo/logo';
 
 type MainPageProps = {
   adsCount: number;
@@ -11,20 +12,7 @@ export default function MainPage({ adsCount }: MainPageProps): JSX.Element {
       <header className='header'>
         <div className='container'>
           <div className='header__wrapper'>
-            <div className='header__left'>
-              <Link
-                className='header__logo-link header__logo-link--active'
-                to={''}
-              >
-                <img
-                  className='header__logo'
-                  src='img/logo.svg'
-                  alt='6 cities logo'
-                  width='81'
-                  height='41'
-                />
-              </Link>
-            </div>
+            <Logo />
             <nav className='header__nav'>
               <ul className='header__nav-list'>
                 <li className='header__nav-item user'>
