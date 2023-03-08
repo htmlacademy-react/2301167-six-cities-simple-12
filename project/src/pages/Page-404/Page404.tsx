97cc100efd442af0a4471fd24e0900e9e45b128a
MOROZOVcode <morozov.code@gmail.com>
2023-03-08 12:27:37 +0400

    Добавляет страницу Page404

---
+++ project/src/pages/Page-404/Page404.tsx
@@ -0,0 +1,11 @@
+import './style404.css';
+
+export default function Page404(): JSX.Element {
+  return (
+    <>
+      <h1>404</h1>
+      <h2>Note Found</h2>
+      <a href='#'>Go to the main page</a>
+    </>
+  );
+}
