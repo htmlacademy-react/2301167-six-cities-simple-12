2b207c881fb7270063a74b64ccb690f2ff0170b7
MOROZOVcode <morozov.code@gmail.com>
2023-03-21 19:58:16 +0400

    Исправляет замечания по прошлому ПР

--- project/src/const.ts
+++ project/src/const.ts
@@ -14,3 +14,14 @@ export const ZERO_ID = 0;
 
 export const MAX_RATING = 5;
 export const MULTIPLIER_FOR_RATING = 100 / MAX_RATING;
+
+export const ArrRatingStars = [5, 4, 3, 2, 1];
+
+export const ArrCitiesForNav = [
+  'Paris',
+  'Cologne',
+  'Brussels',
+  'Amsterdam',
+  'Hamburg',
+  'Dusseldorf',
+];
