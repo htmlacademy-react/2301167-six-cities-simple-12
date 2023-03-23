2b1ee0139c49aee3362a600f32408070aebc331b
MOROZOVcode <morozov.code@gmail.com>
2023-03-23 14:51:07 +0400

    Исправляет замечания по прошлой ПР

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import LocationList from '../../components/location-list/location-list';
 import OffersListContainer from '../../components/offers-list-container/offers-list-container';
 import { Offers, City } from '../../types/offers-type';
 
@@ -12,43 +12,7 @@ export default function MainPage({ offers, city }: MainPageProps): JSX.Element {
     <main className='page__main page__main--index'>
       <h1 className='visually-hidden'>Cities</h1>
       <div className='tabs'>
-        <section className='locations container'>
-          <ul className='locations__list tabs__list'>
-            <li className='locations__item'>
-              <Link className='locations__item-link tabs__item' to='#'>
-                <span>Paris</span>
-              </Link>
-            </li>
-            <li className='locations__item'>
-              <Link className='locations__item-link tabs__item' to='#'>
-                <span>Cologne</span>
-              </Link>
-            </li>
-            <li className='locations__item'>
-              <Link className='locations__item-link tabs__item' to='#'>
-                <span>Brussels</span>
-              </Link>
-            </li>
-            <li className='locations__item'>
-              <Link
-                className='locations__item-link tabs__item tabs__item--active'
-                to={''}
-              >
-                <span>Amsterdam</span>
-              </Link>
-            </li>
-            <li className='locations__item'>
-              <Link className='locations__item-link tabs__item' to='#'>
-                <span>Hamburg</span>
-              </Link>
-            </li>
-            <li className='locations__item'>
-              <Link className='locations__item-link tabs__item' to='#'>
-                <span>Dusseldorf</span>
-              </Link>
-            </li>
-          </ul>
-        </section>
+        <LocationList />
       </div>
       <div className='cities'>
         <OffersListContainer offers={offers} city={city} />
