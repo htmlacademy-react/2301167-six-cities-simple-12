a09d1def862adeef9faf075305eacd3a4ccd5eb5
MOROZOVcode <morozov.code@gmail.com>
2023-03-27 16:36:42 +0400

    Изменит цвет маркеров на карте

--- project/src/const.ts
+++ project/src/const.ts
@@ -33,8 +33,5 @@ export const OPTIONS_SORTING = [
   'Top rated first',
 ];
 
-export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
-
-export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+export const URL_MARKER_DEFAULT = '../img/pin.svg';
+export const URL_MARKER_CURRENT = '../img/pin-active.svg';
