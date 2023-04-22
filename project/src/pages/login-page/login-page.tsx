a145888e6bf60a5db7c7efcc9bd3b91ba01f6dee
MOROZOVcode <morozov.code@gmail.com>
2023-04-22 11:50:16 +0400

    Добавит классы странице login-page для правильного отображения

--- project/src/pages/login-page/login-page.tsx
+++ project/src/pages/login-page/login-page.tsx
@@ -53,69 +53,71 @@ export default function LoginPage() {
         <title>6/Cities.Login</title>
       </Helmet>
 
-      <header className='header'>
-        <div className='container'>
-          <div className='header__wrapper'>
-            <Logo />
-          </div>
-        </div>
-      </header>
-
-      <main className='page__main page__main--login'>
-        <div className='page__login-container container'>
-          <section className='login'>
-            <h1 className='login__title' style={{ display: 'flex' }}>
-              Sign in
-            </h1>
-            <form
-              className='login__form form'
-              action='#'
-              method='post'
-              onSubmit={handleSubmit}
-            >
-              <div className='login__input-wrapper form__input-wrapper'>
-                <label className='visually-hidden'>E-mail</label>
-                <input
-                  ref={loginRef}
-                  className='login__input form__input'
-                  type='email'
-                  name='email'
-                  placeholder='Email'
-                  required
-                />
-              </div>
-              <div className='login__input-wrapper form__input-wrapper'>
-                <label className='visually-hidden'>Password</label>
-                <input
-                  ref={passwordRef}
-                  className='login__input form__input'
-                  type='password'
-                  name='password'
-                  placeholder='Password'
-                  required
-                />
-              </div>
-              <button
-                className='login__submit form__submit button'
-                type='submit'
-              >
-                Sign in
-              </button>
-            </form>
-          </section>
-          <section className='locations locations--login locations--current'>
-            <div className='locations__item'>
-              <Link
-                className='locations__item-link'
-                to='#'
-                onClick={(evt) => handleRedirectToCity(evt)}
-              >
-                <span>{getRandomCity}</span>
-              </Link>
+      <div className='page page--gray page--login'>
+        <header className='header'>
+          <div className='container'>
+            <div className='header__wrapper'>
+              <Logo />
             </div>
-          </section>
-        </div>
-      </main>
+          </div>
+        </header>
+
+        <main className='page__main page__main--login'>
+          <div className='page__login-container container'>
+            <section className='login'>
+              <h1 className='login__title' style={{ display: 'flex' }}>
+                Sign in
+              </h1>
+              <form
+                className='login__form form'
+                action='#'
+                method='post'
+                onSubmit={handleSubmit}
+              >
+                <div className='login__input-wrapper form__input-wrapper'>
+                  <label className='visually-hidden'>E-mail</label>
+                  <input
+                    ref={loginRef}
+                    className='login__input form__input'
+                    type='email'
+                    name='email'
+                    placeholder='Email'
+                    required
+                  />
+                </div>
+                <div className='login__input-wrapper form__input-wrapper'>
+                  <label className='visually-hidden'>Password</label>
+                  <input
+                    ref={passwordRef}
+                    className='login__input form__input'
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                    required
+                  />
+                </div>
+                <button
+                  className='login__submit form__submit button'
+                  type='submit'
+                >
+                  Sign in
+                </button>
+              </form>
+            </section>
+            <section className='locations locations--login locations--current'>
+              <div className='locations__item'>
+                <Link
+                  className='locations__item-link'
+                  to='#'
+                  onClick={(evt) => handleRedirectToCity(evt)}
+                >
+                  <span>{getRandomCity}</span>
+                </Link>
+              </div>
+            </section>
+          </div>
+        </main>
+      </div>
     </>
   );
 }
