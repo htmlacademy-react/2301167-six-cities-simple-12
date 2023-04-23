19434a99c337e0bb7bf6d66f27f346649a05d0ee
MOROZOVcode <morozov.code@gmail.com>
2023-04-23 12:22:04 +0400

    Поправит мелкие замечания

--- project/src/components/offers-list-container/offers-list-container.tsx
+++ project/src/components/offers-list-container/offers-list-container.tsx
@@ -2,12 +2,11 @@ import { useEffect, useState } from 'react';
 import PlacesSorting from '../places-sorting/places-sorting';
 import CitiesPlacesList from '../cities-places-list/cities-places-list';
 import Map from '../map/map';
-import { Offer, Offers } from '../../types/offers-type';
+import { City, Offer, Offers } from '../../types/offers-type';
 import { useAppSelector } from '../../hooks';
 import {
   getErrorOffersStatus,
   getOffers,
-  getOffersDataLoadingStatus,
 } from '../../store/app-data/app-data.selectors';
 import { toast } from 'react-toastify';
 import { getCurrentCity } from '../../store/app-process/app-process.selectors';
@@ -16,7 +15,9 @@ import LoadingPage from '../../pages/loading-page/loading-page';
 
 const getOffersOfcity = (offers: Offers, city: string) =>
   offers.filter((offer) => offer.city.name === city);
-const getCityForMap = (offers: Offers) => offers[0].city;
+
+const getCityForMap = (offers: Offers, city: string) =>
+  offers.find((offer) => offer.city.name === city)?.city;
 
 export default function OffersListContainer(): JSX.Element {
   const [activeOffer, setActiveOffer] = useState<Offer | undefined>(undefined);
@@ -25,7 +26,6 @@ export default function OffersListContainer(): JSX.Element {
   const offers = useAppSelector(getOffers);
   const city = useAppSelector(getCurrentCity);
 
-  const isOffersLoading = useAppSelector(getOffersDataLoadingStatus);
   const isErrorLoading = useAppSelector(getErrorOffersStatus);
 
   useEffect(() => {
@@ -39,7 +39,7 @@ export default function OffersListContainer(): JSX.Element {
     setRelevantOffers(currentOffers);
   }, [city, offers]);
 
-  if (!isOffersLoading && !isErrorLoading && !relevantOffers.length) {
+  if (!relevantOffers.length) {
     return <MainEmpty city={city} />;
   }
 
@@ -52,8 +52,8 @@ export default function OffersListContainer(): JSX.Element {
       <div className='cities__places-container container'>
         <section className='cities__places places'>
           <h2 className='visually-hidden'>Places</h2>
-          <b className='places__found'>
-            {relevantOffers.length} places to stay in Amsterdam
+          <b className='places__found' style={{ textAlign: 'left' }}>
+            {relevantOffers.length} places to stay in {city}
           </b>
           <PlacesSorting />
           <CitiesPlacesList
@@ -63,7 +63,7 @@ export default function OffersListContainer(): JSX.Element {
         </section>
         <div className='cities__right-section'>
           <Map
-            city={getCityForMap(relevantOffers)}
+            city={getCityForMap(offers, city) as City}
             offers={relevantOffers}
             activeOffer={activeOffer}
             className={'cities'}
